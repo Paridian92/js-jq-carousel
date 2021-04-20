b7ad657a48d51cb520b5bd20a83ca5814dbe1710
Paridian92 <paridian92@hotmail.it>
2021-04-20 16:38:28 +0200

    messi i font awesome

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  var burgermenu = $('.hamburger-menu');
+  var aperto = true 
+
+  $('i').click(function(){
+    if(aperto){
+      burgermenu.fadeOut();
+    }else{
+      burgermenu.fadeIn()
+    }
+    aperto = !aperto
+    
+  })
+
+})
+
+
+
